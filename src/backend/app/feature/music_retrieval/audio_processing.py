dfab537da864a27a7621631b4cbd57bd073410d2
Fozzzzy <122066488+andrewtedja@users.noreply.github.com>
2024-12-02 15:26:45 +0700

    feat: added save dataset similarity and get similar file

--- src/backend/app/feature/music_retrieval/audio_processing.py
+++ src/backend/app/feature/music_retrieval/audio_processing.py
@@ -4,42 +4,18 @@ import os
 from mido import MidiFile
 import matplotlib.pyplot as plt
 
-# PREPROCESS WAV
-def preprocess_wav(file_path):
-    audio_data, sample_rate = librosa.load(file_path)
+from audio_load import preprocess_midi, preprocess_wav
 
-    # Get pitches from wav file
-    pitches = librosa.yin(audio_data, fmin=librosa.note_to_hz('C1'), fmax=librosa.note_to_hz('C8'), sr=sample_rate)
-
-    # Convert pitch (Hz) to MIDI note numbers
-    pitch_data = []
-    for pitch in pitches:
-        if pitch > 0: 
-            pitch_data.append(int(librosa.hz_to_midi(pitch)))
-        else:
-            pitch_data.append(0)  
-    return pitch_data
-    
-# PREPROCESS MIDI
-def preprocess_midi(file_path):
-    midi = MidiFile(file_path)
-    pitch_data = []
-
-    for track in midi.tracks:
-        for msg in track:
-            if msg.type == 'note_on' and msg.velocity > 0:
-                pitch_data.append(msg.note)
-    return pitch_data
-
-# APPLY SLIDING WINDOW
-def apply_sliding_window(data, window, step):
+# Windowing
+def apply_sliding_window(data, window, step): 
     segments = []
+    
     for i in range(0, len(data) + 1 - window, step):
         segment = data[i:i + window]
         segments.append(segment)
     return segments;
 
-# MAIN AUDIO PROCESSING FUNCTION
+# Audio Processing
 def get_processed_audio(file_path):
     if file_path.endswith('.wav'):
         pitch_data = preprocess_wav(file_path)
@@ -55,23 +31,23 @@ def get_processed_audio(file_path):
         print("Pitches list is empty.")
         return pitch_data
 
-    # Normalisasi Tempo
-    mu = np.mean(pitches)
-    sigma = np.std(pitches)
+    # # Normalisasi Pitch (Opsional)
+    # mu = np.mean(pitches)
+    # sigma = np.std(pitches)
     
-    if (sigma == 0):
-        sigma = 1
+    # if (sigma == 0):
+    #     sigma = 1
 
-    processed_pitch_data = []
-    for p in pitch_data:
-        if p > 0:
-            normalized_value = (p - mu) / sigma
-            processed_pitch_data.append(normalized_value)
-        else:
-            processed_pitch_data.append(0)
-    return processed_pitch_data
+    # processed_pitch_data = []
+    # for p in pitch_data:
+    #     if p > 0:
+    #         normalized_value = (p - mu) / sigma
+    #         processed_pitch_data.append(normalized_value)
+    #     else:
+    #         processed_pitch_data.append(0)
+    return segments
 
-# CREATE AND NORMALIZE HISTOGRAM
+# Create Histogram and Normalize
 def create_and_normalize_histogram(data, num_bins, val_range = None):
     # Create
     if val_range is None:
@@ -86,26 +62,23 @@ def create_and_normalize_histogram(data, num_bins, val_range = None):
         print("Error Warning: Histogram sum 0 atau negatif")
         return histogram
 
-# EXTRACT FEATURES
+# Extract Features
 def extract_features(pitch_data):
+    
     # ATB [0, 127]
     bins_atb = 128
     atb_normalized = create_and_normalize_histogram(pitch_data, bins_atb)
 
-
     # RTB [-127, 127] (selisih antara nada-nada berurutan)
     bins_rtb = 255
-    for i in range(1, len(pitch_data)):
-        rtb_data = pitch_data[i] - pitch_data[i - 1]
-        rtb_normalized = create_and_normalize_histogram(rtb_data, bins_rtb, (-127, 127))
+    rtb_data = [pitch_data[i] - pitch_data[i-1] for i in range(1, len(pitch_data))]
+    rtb_normalized = create_and_normalize_histogram(rtb_data, bins_rtb, (-127, 127))
     
     # FTB [-127, 127] (selisih antara nada-nada dengan nada pertama)
     bins_ftb = 255
-    if len(pitch_data > 0):
-        for i in range(1, len(pitch_data)):
-            first_tone = pitch_data[0]
-            ftb_data = pitch_data[i] - first_tone
-            ftb_normalized = create_and_normalize_histogram(ftb_data, bins_ftb, (-127, 127))
+    if len(pitch_data) > 0:
+        ftb_data = [pitch_data[i] - pitch_data[0] for i in range(1, len(pitch_data))]
+        ftb_normalized = create_and_normalize_histogram(ftb_data, bins_ftb, (-127, 127))
     else:
         ftb_normalized = np.zeros(255, dtype=float)
 
@@ -114,7 +87,7 @@ def extract_features(pitch_data):
 
     return vector_combined_features
 
-# GET COSINE SIMILARITY
+# Cosine Similarity
 def get_cosine_similarity(vector_A, vector_B):
     dot_product = np.dot(vector_A, vector_B)
     norm_vector_A = np.linalg.norm(vector_A)
@@ -128,29 +101,56 @@ def get_cosine_similarity(vector_A, vector_B):
     cosine_similarity = dot_product / norm_product
     return cosine_similarity
 
-# MAIN
-def main():
-    # upload_audio_path = "src/backend/app/feature/music_retrieval/music_files/twinklemidi.mid"
-    upload_audio_path = "test/music_files/twinklewav.wav"
-    dataset_audio_path = "test//twinklemidi.mid"
-
-    if (os.path.exists(upload_audio_path)) and (os.path.exists(dataset_audio_path)):
-        print("File exists (upload):", os.path.exists(upload_audio_path))
-        print("File exists (dataset):", os.path.exists(dataset_audio_path))
+# Memproses data-data gambar dataset
+def save_dataset_similarity(query_path, dataset_folder):
+    dataset_similarity_list = [] # Bentuk tuple
+    if (os.path.exists(query_path)) and (os.path.exists(dataset_folder)):
+        # Check if file exists
+        print(f"Query file exists {query_path}:", os.path.exists(query_path))
+        print(f"Folder exists {dataset_folder}):", os.path.exists(dataset_folder))
         
-        # Preprocess wav/midi -> Get processed audio -> Extract features -> Get cosine similarity
-        upload_processed_pitch_data = get_processed_audio(upload_audio_path)
-        dataset_processed_pitch_data = get_processed_audio(dataset_audio_path)
+        # Testing
+        for dataset_file in os.listdir(dataset_folder):
+            dataset_path = os.path.join(dataset_folder, dataset_file)
 
-        vector_upload = extract_features(upload_processed_pitch_data)
-        vector_dataset = extract_features(dataset_processed_pitch_data)
+            if os.path.exists(dataset_path):
+                print(f"Dataset file exists ({dataset_file}):", os.path.exists(dataset_path))
 
-        cosine_similarity = get_cosine_similarity(vector_upload, vector_dataset)
-        print(f"Cosine Similarity: {cosine_similarity}")
+                # Preprocess wav/midi -> Get processed audio -> Extract features -> Get cosine similarity
+                query_processed_pitch_data = get_processed_audio(query_path)
+                dataset_processed_pitch_data = get_processed_audio(dataset_path)
+
+                vector_query = extract_features(query_processed_pitch_data)
+                vector_dataset = extract_features(dataset_processed_pitch_data)
+                cosine_similarity = get_cosine_similarity(vector_query, vector_dataset)
+
+                dataset_similarity_list.append((dataset_file, cosine_similarity))
+            else:
+                print("Gada Filenya")
     else:
-        print(f"Gada Filenya")
+        print(f"Gada Foldernya")
 
-# TESTING
+    return dataset_similarity_list
+
+def get_similar_file(dataset_similarity_list):
+    dataset_similarity_list.sort(key=lambda x: x[1], reverse=True)
+    if (dataset_similarity_list):
+        most_similar_file, cosine_similarity = dataset_similarity_list[0]
+        return most_similar_file, cosine_similarity
+
+# Main
+def main():
+    # query_path = "src/backend/app/feature/music_retrieval/music_files/twinklemidi.mid"
+    query_path = "test/music_query/Speaker_0000_00000.wav"
+    dataset_folder = "test/music_dataset"
+
+    # Testing
+    dataset_similar = save_dataset_similarity(query_path, dataset_folder)
+    most_similar_file, cosine_similarity = get_similar_file(dataset_similar)
+
+    print(f"Dataset Similarity: {dataset_similar}")
+    print(f"Most similar file: {most_similar_file}")
+    print(f"Cosine similarity: {cosine_similarity}")
+
 if __name__ == "__main__":
     main()
-
